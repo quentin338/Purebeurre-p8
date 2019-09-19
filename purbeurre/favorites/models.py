fdb88b7244fccb99fb1fcbdc253a0ef70e61853c
quentin338 <quentin.bertrand@yahoo.fr>
2019-09-19 14:56:08 +0200

    Modified models. Added migrations.

--- purbeurre/favorites/models.py
+++ purbeurre/favorites/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Favorite(models.Model):
     date = models.DateTimeField(auto_now_add=True)
-    # user_id = models.ForeignKey('Users.user', on_delete=models.CASCADE)
+    user_id = models.ForeignKey('users.User', on_delete=models.CASCADE)
     new_product_id = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name="new_product")
     ancient_product_id = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name="ancient_product")
