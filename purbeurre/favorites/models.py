523233a830a21f348990d161e46abb00868db4bf
quentin338 <quentin.bertrand@yahoo.fr>
2019-10-07 17:17:04 +0200

    Modified User to inherit AbstractBaseUser. Added UserManager. Modified favorites models to give more flexibility.

--- purbeurre/favorites/models.py
+++ purbeurre/favorites/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 
 class Favorite(models.Model):
     date = models.DateTimeField(auto_now_add=True)
-    user_id = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     new_product_id = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name="new_product")
     ancient_product_id = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name="ancient_product")
