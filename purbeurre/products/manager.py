8bba117a7f68698385b414701818dc1b475b3bb2
Quentin Bertrand <quentin.bertrand@yahoo.fr>
2020-05-21 12:02:17 +0200

    Added capitalize() for product search if no result.

--- purbeurre/products/manager.py
+++ purbeurre/products/manager.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class ProductManager(models.Manager):
     def search_autocomplete(self, user_search):
-        # results = Product.objects.filter(name__icontains=user_search).order_by('-nutriscore')[:10]
         results = super().get_queryset().filter(name__icontains=user_search) \
                       .order_by('-nutriscore')[:10]
 
@@ -12,9 +11,9 @@ class ProductManager(models.Manager):
         return results
 
     def get_old_product(self, user_search):
-        # old_product = super().get_queryset().filter(name=user_search).first()
         old_product = super().get_queryset().filter(models.Q(name=user_search) |
-                                                    models.Q(name__icontains=user_search))\
+                                                    models.Q(name__icontains=user_search) |
+                                                    models.Q(name__icontains=user_search.capitalize()))\
                                             .first()
 
         return old_product
