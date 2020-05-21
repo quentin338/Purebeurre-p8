d1208bc259bbd82eb2aa20f6c11d009dec113386
Quentin Bertrand <quentin.bertrand@yahoo.fr>
2020-05-21 19:51:16 +0200

    Trying to move around csrftoken.js

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'purbeurre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "core/templates/core")],
+        'DIRS': [os.path.join(BASE_DIR, "core/templates/")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
