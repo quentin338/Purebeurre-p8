30758ed3cfd0be637d4d8a5da243fcbfeecf71d7
Quentin Bertrand <quentin.bertrand@yahoo.fr>
2020-05-21 19:36:20 +0200

    Modifying TEMPLATES as heroku is not taking it.

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'purbeurre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "core/templates/")],
+        'DIRS': [os.path.join(BASE_DIR, "core/templates/core")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
